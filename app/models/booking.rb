47c1b4f12bc1c0d64d463d2c65cc089c85b9b817
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-26 18:36:35 +0200

    add validations on booking and offer model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   belongs_to :customer, class_name: "User", foreign_key: "customer_id"
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,4 @@
 class Offer < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
+  validates :name, presence: true
 end
