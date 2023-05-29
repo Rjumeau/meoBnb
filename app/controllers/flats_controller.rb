0435ea1ab527405d495f76edb57cb490baced9ae
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-29 10:58:21 +0200

    new and create action done for flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,26 @@
 class FlatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
   def new
     @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.owner = current_user
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :price, :address)
+    params.require(:flat).permit(:name, :description, :price, :address, :photo)
   end
 end
