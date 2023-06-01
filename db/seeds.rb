ad36f19719205428325a5c585d5cb7485963c21d
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-06-01 17:14:49 +0200

    add logo and spring

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Flat.destroy_all
+
+puts "Create users..."
+
+User.create(email: "cat1@meow.com", password: "secrette", first_name: "Simba")
+User.create(email: "cat2@meow.com", password: "secrette", first_name: "Nala")
+
+puts "Done !"
+
+puts "Create flats..."
+Flat.create(
+  owner_id: 1,
+  name: "Maison avec jardin d'herbes à chats",
+  description: "Proches de toutes commodités, Maison de mon humain
+                possède un jardin rempli d'herbe à chat de qualitey",
+  price: 130
+)
+
+10.times do
+  Flat.create(
+    owner_id: 2,
+    name: "Appartement avec griffoir géant",
+    description: "Proches de toutes commodités, l'appartement de mon humain
+                  possède un griffoir géant pour le plaisir de vos griffes",
+    price: 55
+  )
+end
+
+puts "Done !"
+
+puts "Finished !"
